f6519d0997654355ea890c662aab6dab87c0acf3
Vasilisa Nichipor <vsilfox@gmail.com>
2022-10-26 09:01:50 +0300

    fixed schema impl

--- src/ru/netology/graphics/image/TextColorSchemaImpl.java
+++ src/ru/netology/graphics/image/TextColorSchemaImpl.java
@@ -5,12 +5,6 @@ public class TextColorSchemaImpl implements TextColorSchema {
     @Override
     public char convert(int color) {
         char[] chars = {'#', '$', '@', '%', '*', '+', '-', '\''};
-
-        for (int i = 0; i < chars.length; i++) {
-            if (color / 32 == i) {
-                return chars[i];
-            }
-        }
-        return '0';
+        return chars[color / (256 / chars.length)];
     }
 }
